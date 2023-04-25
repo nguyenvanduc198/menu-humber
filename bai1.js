80b110ce6d5113af25bf1d78cf65e879561633e6
nguyenvanduc198 <nguyenmanh.vanduc@gmail.com>
2023-04-25 21:18:13 +0900

    add-code

--- bai1.js
+++ bai1.js
@@ -32,3 +32,26 @@ function removeShow() {
 tabItems.forEach(item => {
   item.addEventListener('click', selectItem);
 });
+
+
+
+(function() {
+  "use strict";
+
+  fb.events.form.mounted = [function (state) {
+    // フォームのコンテンツ部分の要素を取得します
+    var formContent = document.getElementsByClassName('fb-content')[0];
+
+    // ボタンを作成します
+    var submitButton = document.createElement('input');
+    submitButton.type = 'button';
+    submitButton.value = 'submit';
+    // ボタンが押された際に現在のフォームを強制的に回答するように設定します
+    submitButton.onclick = fb.forceSubmitForm;
+
+    // 作成したボタンをフォームのコンテンツ部分の要素の先頭に追加します
+    formContent.insertBefore(submitButton, formContent.firstChild);
+
+    return state;
+  }];
+})();
